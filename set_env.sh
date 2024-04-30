cd0e2dab2c296b9dbacb2b7ddb46b454d42f490b
Nguyễn Xuân Tiến <tiennx@Nguyens-Laptop.local>
2024-04-30 09:57:07 +0700

    update

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=mypassword
+export POSTGRES_HOST=database-1.c8t3ee6kqwll.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::my-681930916209-bucket
+export AWS_REGION=us-east-1b
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
---
+++ udagram-api-feed/src/sequelize.ts
@@ -0,0 +1,19 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+console.log(config);
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+  'dialectOptions': {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
+  'dialect': config.dialect,
+  'storage': ':memory:',
+});
