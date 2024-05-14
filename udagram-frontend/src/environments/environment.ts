8dc85d4c85a2d6d9158ad575f271fbdeed4f0567
Tiennx <tiennxbusiness@gmail.com>
2024-05-14 15:27:56 +0700

    update

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a123de4db114c441289dfacff2035b4d-58736389.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
